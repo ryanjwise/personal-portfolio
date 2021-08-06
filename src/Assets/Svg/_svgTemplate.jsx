25ae0106b8abed43b1b9b7016d0d6f3f73948dbb
Ryan Wise <ryan.jeffwise@gmail.com>
2021-08-06 22:08:26 +1000

    Add SVG components

---
+++ src/Assets/Svg/_svgTemplate.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Github({ className }) {
+  return (
+    <svg
+      className={className}
+
+    >
+      <g
+        fill="currentColor"
+      >
+        <path d=""></path>
+      </g>
+    </svg>
+  );
+}

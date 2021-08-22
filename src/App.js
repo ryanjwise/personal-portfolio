a287967f65f0c5bdfcfb7c697788562221f9567d
Ryan Wise <ryan.jeffwise@gmail.com>
2021-08-22 18:09:21 +1000

    Update home route to about

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Projects from "./Pages/Projects";
-import './Styles/styles.scss';
+import "./Styles/styles.scss";
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
           <Route path="/contact">
             <Contact />
           </Route>
           <Route path="/projects">
             <Projects />
+          </Route>
+          <Route path="/">
+            <About />
           </Route>
         </Switch>
         <Footer />
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <nav>
       <ul>
-        <li><Link to="/about">About me</Link></li>
+        <li><Link to="/">About me</Link></li>
         <li><Link to="/contact">contact</Link></li>
         <li><Link to="/projects">projects</Link></li>
         {/* <li><a href="./blog.html">blog</a></li> */}

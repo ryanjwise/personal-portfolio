80cdc88ca614da27458173be2eb147d0b0e953d2
Ryan Wise <ryan.jeffwise@gmail.com>
2021-08-22 22:11:34 +1000

    Update Socials svg paths

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Icons from "../Assets/Svg/Icons";
+import SocialIcons from "../Assets/Svg/SocialIcons";
 
 export default function Socials() {
   return (
     <nav className="socials">
       <a href="https://github.com/ryanjwise">
-        <Icons className="social-link" icon="github" />
+        <SocialIcons className="social-link" icon="github" />
       </a>
       <a href="https://twitter.com/RyanJWise">
-        <Icons className="social-link" icon="twitter" />
+        <SocialIcons className="social-link" icon="twitter" />
       </a>
       <a href="https://www.linkedin.com/in/ryanjwise/">
-        <Icons className="social-link" icon="linkedin" />
+        <SocialIcons className="social-link" icon="linkedin" />
       </a>
     </nav>
   );
